f4fcff0e38811463a6bcdae62c7fc910b444d1ed
RKPatel24 <kaneriyaraj133@gmail.com>
2022-12-25 02:23:38 +0530

    Assignment

---
+++ C Programming/Assignment/assignments.(1)/assignments.c
@@ -0,0 +1,63 @@
+1) What is a shofteare? What is software engineering?
+
+Shoftware is a set of instruction,data or programs that tell a computer.
+Shoftwere is that part of a computer, which can not ne touched. shoftwere tell a computer what to do and how to do it.
+Shoftwere engineering is a technique through which we can developed or created shoftwere foe computer system and any other electronic devices.
+Shoftwere engineering is a process in which used need are analyzes and shoftwere is designed based on there needs.
+In Shoftwere engineering the development of shoftwere using well define scientific principle, method and procedures.
+shoftwere engineering build these Shoftwere and application by using designing and progreming languages.
+   In order to create a complex shoftwere we should use shoftwere engineering techique as well as to reduce the comlexility we should use abstraction
+snd decomposition where abstraction describes only importent part of the shoftwere and leave irrelevent things for later stage of development so the
+requrement of shoftwere be come simple. And decomposition breakdown the softwere into no of module where each module produces a well defined independent
+task.
+
+2) Explain types of shoftwere.
+    1) system softwere
+    2) Application softwere
+    3) utility software
+   
+   1) system softwere :- system softwere is a softwere designed to provide a platgrom to other softwere. system sotwere control and manage the operation
+                         of computer hardware.
+                        Ex : Operating syatem (windows,Android,linux etc...)
+   2) Application:- The softwere that helps you to do a specific type of work is called application softwere.
+                    Ex : (Ms word, Excel etc....)
+   3) utility softwere :- Utility softwere helps to manage, maintain and control computer resorces.
+                          Ex : antivirus softwere, backup softwere, Disk tools etc...)
+
+3) what is SDLC? Explain each phase of SDLC
+   SDLC :- The softwere developement Life cycle.
+           The softwere developement life cycle is a structured process that enables the production of high-quility,lowcost softwere, in the shortest possible
+           production time. the goal of the SDLC is to produce superior softwere that meets and exceeds all customer expectation and demands.
+           
+   There are 7 step in SDLC.
+
+          1) Requirements :- In order understand what requirements are, it is necessary to know why one needs them. Basically, requirements are statements that 
+                             indicate what a system needs to do in order to provide a capability. Requirements are generally prepared duringg the early stages 
+                             of a projects system development lifecycle.
+          2) Analysis :- An analysis is just the process of breaking something down and figuring out how it works.
+          3) Designing :- Design means to create a plan or sketch of something that is going to be made later, especially a plan that details what the finished 
+                          thing will do and look like. A design is the plan or sketch created as a result of this activity. Design also means to intend something
+                          for a specific purpose.
+          4) Development :- Development is the process of growth of a particular thing.
+                            In economics it is related to infrastructure, resources including human resource and natural resources.
+          5) Testing :- The testing phase of the software development lifecycle is where you focus on investigation and discovery. During the testing phase, 
+                        developers find out whether their code and programming work according to customer requirements.
+          6) deployment :- The deployment phase is the final phase of the software development life cycle and puts the product into production. After the project 
+                            team tests the product and the product passes each testing phase, the product is ready to go live.
+          7) Maintenance :- The maintenance phase of the SDLC occurs after the product is in full operation. Maintenance of software can include software upgrades, 
+                            repairs, and fixes of the software if it breaks.
+                            Software applications often need to be upgraded or integrated with new systems the customer deploys.
+
+
+4) What is DFD? Create a DFD diagram on Flipkart
+
+         DFD means "Data flow Diagram.
+         It is also known as Bubble chart through which we can represent the flow of data graphically in an information system.
+         By using DFD We can easily understand the overall functionality of system because diagram represents the incoming data flow outgoing data flow and stored data in a graphical from.
+         It describes how data is processed in a system in them of input and output.
+         A DFD model uses a no. of notations or symbol to represent flow of data.
+
+5) What is Flow chart? Create a flowchart to make addition of two numbers
+
+        Flowchart is a symbolic or diagrammatic representation of an algorithm. It uses several geometrical figures to represent the operation, and arrow to show the direction of flow. 
+
