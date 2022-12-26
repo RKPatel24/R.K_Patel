0d456402a6114d26572f53144a51efd257a5ef5a
RKPatel24 <kaneriyaraj133@gmail.com>
2022-12-26 11:40:54 +0530

    new task ADD

---
+++ C Programming/Classwork/26-12-2022/Acceptdemo.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main ()
+
+{
+	int num1, num2;
+	printf("\nEnter Number 1 :");
+	scanf("%d",&num1);
+	printf("\nEnter Number 2 :");
+	scanf("%d",&num2);
+	
+	printf("Number1 = %d \nNumbar2 = %d",num1,num2);
+	//printf("\nnumber2 = %d",num2);
+	printf("\nAddition : %d",(num1+num2));
+	printf("\nSubtraction : %d",(num1-num2));
+	printf("\nMultiplication : %d",(num1*num2));
+	printf("\nDivision : %.2f",((float)num1/num2));
+}
